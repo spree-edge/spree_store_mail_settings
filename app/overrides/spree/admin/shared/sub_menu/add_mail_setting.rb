58d55b33d65006eba8226152a4ecb9e951c20748
Vishal <vishal@bluebash.co>
2023-08-07 18:19:50 +0530

    upgraded with spree 4.6 and removed the deface dependency

--- app/overrides/spree/admin/shared/sub_menu/add_mail_setting.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-    virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-    name: 'add_mail_setting',
-    insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-    text: "<%= configurations_sidebar_menu_item Spree.t(:mail_setting), edit_admin_email_settings_path %>"
-)
